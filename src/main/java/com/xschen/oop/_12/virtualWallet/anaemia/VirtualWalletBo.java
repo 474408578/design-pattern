191ea99e488d72d83d4ad819a810902554e9e51b
xschen <xschen@trip.com>
2020-09-22 20:26:06 +0800

    add virtual wallet system

---
+++ src/main/java/com/xschen/oop/_12/virtualWallet/anaemia/VirtualWalletBo.java
@@ -0,0 +1,14 @@
+package com.xschen.oop._12.virtualWallet.anaemia;
+
+import java.math.BigDecimal;
+
+/**
+ * @author xschen
+ */
+
+
+public class VirtualWalletBo {
+    private Long id;
+    private Long createTime;
+    private BigDecimal balance;
+}
---
+++ src/main/java/com/xschen/oop/_12/virtualWallet/anaemia/VirtualWalletEntity.java
@@ -0,0 +1,38 @@
+package com.xschen.oop._12.virtualWallet.anaemia;
+
+import java.math.BigDecimal;
+
+/**
+ * @author xschen
+ */
+
+
+public class VirtualWalletEntity {
+    private Long id;
+    private Long createTime;
+    private BigDecimal balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
